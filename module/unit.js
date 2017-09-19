84064ec3da7590c2d831d6673f4975a7ea99a839
shenbaoshi <1041982179@qq.com>
2017-09-19 20:54:44 +0800

    提交request

---
+++ module/unit.js
@@ -0,0 +1,14 @@
+//define(function(){
+//
+//	return {
+//		colorRange :[1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"],
+//		randomColor:function(){
+//
+//			return {
+//          	color: "#" + this.colorRange[Math.floor(Math.random() * 15)] + this.colorRange[Math.floor(Math.random() * 15)] + this.colorRange[Math.floor(Math.random() * 15)] + this.colorRange[Math.floor(Math.random() * 15)] + this.colorRange[Math.floor(Math.random() * 15)] + this.colorRange[Math.floor(Math.random() * 15)]
+//      	}
+//		}
+//	}
+//
+//})
+alert("hello world");
